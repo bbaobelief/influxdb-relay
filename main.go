59f8525dea472995b170bbe150a558b44b06052d
bbaobelief <bbaobelief@163.com>
2019-10-14 19:25:51 +0800

    add version info

--- main.go
+++ main.go
@@ -5,28 +5,59 @@ import (
 	"flag"
 	"fmt"
 	"influxdb-relay/common/log"
-	v "influxdb-relay/common/version"
 	"influxdb-relay/config"
 	"influxdb-relay/relay"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"runtime"
 )
 
 var (
 	cfg     = flag.String("config", "", "Configuration file to use")
 	version = flag.Bool("version", false, "show version info")
+
+	gitTag       string = ""
+	gitCommit    string = "$Format:%H$"
+	gitTreeState string = "not a git tree"
+	buildDate    string = "1970-01-01T00:00:00Z"
 )
+
+type Info struct {
+	GitTag       string `json:"gitTag"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	BuildDate    string `json:"buildDate"`
+	GoVersion    string `json:"goVersion"`
+	Compiler     string `json:"compiler"`
+	Platform     string `json:"platform"`
+}
+
+func (info Info) String() string {
+	return info.GitTag
+}
+
+func versionInfo() Info {
+	return Info{
+		GitTag:       gitTag,
+		GitCommit:    gitCommit,
+		GitTreeState: gitTreeState,
+		BuildDate:    buildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
 
 func main() {
 	flag.Parse()
 
 	if *version {
-		v := v.Get()
+		v := versionInfo()
 		marshalled, err := json.MarshalIndent(&v, "", "  ")
 		if err != nil {
-			fmt.Printf("%v\n", err)
+			log.Info.Println(err)
 			os.Exit(1)
 		}
 
