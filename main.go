d4cb14290b15afcf3c8e689975b0e45e4af99182
bbaobelief <bbaobelief@163.com>
2019-10-14 16:15:44 +0800

    添加git版本信息

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"influxdb-relay/common/log"
+	v "influxdb-relay/common/version"
 	"influxdb-relay/config"
 	"influxdb-relay/relay"
 	"net/http"
@@ -12,19 +15,32 @@ import (
 )
 
 var (
-	configFile = flag.String("config", "", "Configuration file to use")
+	cfg     = flag.String("config", "", "Configuration file to use")
+	version = flag.Bool("version", false, "show version info")
 )
 
 func main() {
 	flag.Parse()
 
-	if *configFile == "" {
+	if *version {
+		v := v.Get()
+		marshalled, err := json.MarshalIndent(&v, "", "  ")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(marshalled))
+		return
+	}
+
+	if *cfg == "" {
 		log.Error.Println("Missing configuration file")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	cfg, err := config.LoadConfigFile(*configFile)
+	cfg, err := config.LoadConfigFile(*cfg)
 	if err != nil {
 		log.Error.Println("Problem loading config file:", err)
 	}
